bcf80970e61f622e831f73802e7d4baedc16e6a7
wmcqueensky <mokwinskiwojciech0@gmail.com>
2024-03-17 22:22:46 +0100

    Calculator Program Add

---
+++ PT_Task_0/Calculator/Calculator/Divide.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Divide
+    {
+        public static double Perform(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new ArgumentException("Division by zero is not allowed.");
+            }
+            return num1 / num2;
+        }
+    }
+}
