0b5df79da08dd3a95c578d501cb13a27288a94da
wmcqueensky <mokwinskiwojciech0@gmail.com>
2024-03-17 18:34:58 +0100

    Sample Program Add

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 | Name Surname (initials) | GUID                                     |
 | ----------------------- | ---------------------------------------- |
 | Tymoteusz Piwowarski    | `{F88FF097-6F04-43E1-8BB3-3D26C982D0E1}` |
-| Wojeciech Mokwiński     | `{31675142-1C61-4514-9463-723DCCA6A5FC}` |
+| Wojeciech Mokwiński     | `{EAB98167-303E-4269-BDB3-57C7FACAB61A}` |
---
+++ Sample Program/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sample Program")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sample Program")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c1ab80d8-6358-4e00-9ed3-3c7f643f4541")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
