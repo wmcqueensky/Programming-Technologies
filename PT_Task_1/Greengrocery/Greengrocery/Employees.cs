b58d9e621d8060d5749b16abeb4a4ee13fa50812
247039 Tymoteusz Piwowarski <247039@edu.p.lodz.pl>
2024-04-14 22:48:46 +0200

    User, employees, suppliers and customers classes implemented

---
+++ PT_Task_1/Greengrocery/Greengrocery/Employees.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greengrocery
+{
+    internal class Employees : User
+    {
+        int employeeId;
+        int salary;
+
+        public int getEmployeeId()
+        {
+            return this.employeeId;
+        }
+
+        public void setEmployeeId(int value)
+        {
+            this.employeeId = value;
+        }
+
+        public int getSalary() { return this.salary; }
+
+        public void setSalary(int value) {
+            this.setSalary(value);
+        }
+    }
+}
