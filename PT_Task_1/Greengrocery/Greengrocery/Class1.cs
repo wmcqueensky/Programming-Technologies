62f11476acfd232c233b2715ec4514c6aea33b44
wmcqueensky <mokwinskiwojciech0@gmail.com>
2024-04-14 22:09:47 +0200

    Setup Task 1

---
+++ PT_Task_1/Greengrocery/Greengrocery/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greengrocery
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list to hold grocery items
+            List<string> groceryList = new List<string>();
+
+            // Add some items to the grocery list
+            groceryList.Add("Apples");
+            groceryList.Add("Bananas");
+            groceryList.Add("Tomatoes");
+            groceryList.Add("Onions");
+            groceryList.Add("Potatoes");
+
+            // Display the grocery list
+            Console.WriteLine("My Grocery List:");
+            foreach (string item in groceryList)
+            {
+                Console.WriteLine("- " + item);
+            }
+
+            Console.ReadLine(); // Wait for user input before closing the console window
+        }
+    }
+}
---
+++ PT_Task_1/Greengrocery/GreengroceryTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Greengrocery; // Assuming the program class is in this namespace
+
+namespace GreengroceryTest
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void TestGroceryListContents()
+        {
+            // Arrange
+            Program program = new Program();
+
+            // Act
+        }
+    }
+}
