75ff904ad67987f0bdbdaea11f1119bb7aaafe7a
wmcqueensky <mokwinskiwojciech0@gmail.com>
2024-04-14 22:39:52 +0200

    Add product

---
+++ PT_Task_1/Greengrocery/GreengroceryTest/ProductTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Greengrocery;
+
+namespace GreengroceryTest
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void TestProductProperties()
+        {
+            // Arrange
+            int productId = 1;
+            string productName = "Apple";
+            float productPrice = 1.50f;
+            string productType = "Fruit";
+
+            // Act
+            Product product = new Product(productId, productName, productPrice, productType);
+
+            // Assert
+            Assert.AreEqual(productId, product.Id);
+            Assert.AreEqual(productName, product.GetName());
+            Assert.AreEqual(productPrice, product.GetPrice());
+            Assert.AreEqual(productType, product.GetType());
+        }
+    }
+}
