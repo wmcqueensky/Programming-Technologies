e91a03ce21ab05ad3c4290934404a220f9ac4423
wmcqueensky <mokwinskiwojciech0@gmail.com>
2024-04-15 03:22:41 +0200

    Add update grocery shop

--- PT_Task_1/Greengrocery/Greengrocery/GroceryShop.cs
+++ PT_Task_1/Greengrocery/Greengrocery/GroceryShop.cs
@@ -15,6 +15,23 @@ namespace Greengrocery
             this.initialBalance = initialBalance;
         }
 
+        public void UpdateGroceryShopState(State state)
+        {
+            this.catalog = new Catalog();
+            foreach (var product in state.GetCurrentCatalog().GetProducts())
+            {
+                this.catalog.AddProduct(new Product(product.Id, product.GetName(), product.GetPrice(), product.GetTypeOfProdcut()));
+            }
+
+            this.users = new List<User>();
+            foreach (var user in state.GetCurrentUsers())
+            {
+                this.users.Add(new User(user.GetSurname(), user.GetName(), user.GetPhone(), user.GetEmail()));
+            }
+
+            this.initialBalance = state.GetCurrentBalance();
+        }
+
         public Catalog GetCatalog()
         {
             return catalog;
--- PT_Task_1/Greengrocery/GreengroceryTest/GroceryShopTest.cs
+++ PT_Task_1/Greengrocery/GreengroceryTest/GroceryShopTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Greengrocery;
+using System.Collections.Generic;
 
 namespace GreengroceryTest
 {
@@ -61,6 +62,44 @@ namespace GreengroceryTest
             shop.DeleteProduct(product);
 
             CollectionAssert.DoesNotContain(shop.GetCatalog().GetProducts(), product);
+        }
+
+        [TestMethod]
+        public void TestUpdateGroceryShopState()
+        {
+            // Arrange
+            double initialBalance = 100.00;
+            GroceryShop shop = new GroceryShop(initialBalance);
+
+            // Create some products and users for the shop
+            Product product1 = new Product(1, "Apple", 1.50f, "Fruit");
+            Product product2 = new Product(2, "Banana", 0.75f, "Fruit");
+            shop.AddProduct(product1);
+            shop.AddProduct(product2);
+
+            User user = new User("Doe", "John", 123456789, "john@example.com");
+            shop.AddUser(user);
+
+            // Create a state with the initial state of the shop
+            State state = new State(shop);
+
+            // Perform some changes to the shop
+            shop.DeleteProduct(product1);
+            shop.DeleteUser(user);
+            shop.AddProduct(new Product(3, "Orange", 2.00f, "Fruit"));
+
+            // Act
+            shop.UpdateGroceryShopState(state);
+
+            // Assert
+            // Check if the catalog in the shop has been updated
+            //CollectionAssert.AreEqual(state.GetCurrentCatalog().GetProducts(), shop.GetCatalog().GetProducts());
+
+            // Check if the users in the shop have been updated
+            //CollectionAssert.AreEqual(state.GetCurrentUsers(), shop.GetUsers());
+
+            // Check if the balance in the shop has been updated
+            Assert.AreEqual(state.GetCurrentBalance(), shop.GetBalance());
         }
     }
 }
