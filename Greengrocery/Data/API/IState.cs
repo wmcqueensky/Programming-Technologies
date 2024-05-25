da2e8ab408ce414a3cc0b0cfbfa1eb70862f46e1
247039 Tymoteusz Piwowarski <247039@edu.p.lodz.pl>
2024-05-26 00:34:09 +0200

    interfaces event and state

---
+++ Greengrocery/Data/API/IState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greengrocery.Data.API
+{
+    internal interface IState
+    {
+        
+        int state_id { get; set; }
+
+        int catalog_id { get; set; }
+
+
+    }
+}
