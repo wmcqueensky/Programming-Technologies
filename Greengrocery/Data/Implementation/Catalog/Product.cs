1a0f370af1a8448d11417520f02f007111a7d56f
wmcqueensky <mokwinskiwojciech0@gmail.com>
2024-05-25 23:44:51 +0200

    Change dirs

---
+++ Greengrocery/Data/Implementation/Catalog/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Greengrocery
+{
+    public class Product
+    {
+        private readonly int product_id;
+        private readonly string name;
+        private float price;
+        private readonly string typeOfProduct;
+        public Product(int productId, string name, float price, string type)
+        {
+            this.product_id = productId;
+            this.name = name;
+            this.price = price;
+            this.typeOfProduct = type;
+        }
+
+        public int Id
+        {
+            get { return product_id; }
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public float GetPrice()
+        {
+            return price;
+        }
+
+        public void SetPrice(float newPrice)
+        {
+            if (newPrice < 0)
+            {
+                throw new ArgumentException("Price cannot be negative.");
+            }
+            price = newPrice;
+        }
+
+        public string GetTypeOfProdcut()
+        {
+            return typeOfProduct;
+        }
+    }
+}
