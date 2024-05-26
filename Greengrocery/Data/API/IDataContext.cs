16469022759ed999784894106bb4afe9531cbc6f
wmcqueensky <mokwinskiwojciech0@gmail.com>
2024-05-26 15:27:45 +0200

    Update DataContext

--- Greengrocery/Data/API/IDataContext.cs
+++ Greengrocery/Data/API/IDataContext.cs
@@ -12,9 +12,21 @@ namespace Greengrocery.Data.API
         Task AddCatalogEntryAsync(ICatalog catalogEntry);
         Task RemoveCatalogEntryAsync(int id);
 
-        IQueryable<IUser> User { get; }
-        Task AddUserAsync(IUser userEntry);
-        Task RemoveUserAsync(int id);
+        IQueryable<ICustomer> Customer { get; }
+        Task AddCustomerAsync(ICustomer userEntry);
+        Task RemoveCustomerAsync(int id);
+
+        IQueryable<IEmployee> Employee { get; }
+        Task AddEmployeeAsync(IEmployee userEntry);
+        Task RemoveEmployeeAsync(int id);
+
+        IQueryable<ISupplier> Supplier { get; }
+        Task AddSupplierasync(ISupplier userEntry);
+        Task RemoveSupplierAsync(int id);
+
+        IQueryable<IProduct> Product { get; }
+        Task AddProductasync(IProduct userEntry);
+        Task RemoveProductAsync(int id);
 
         IQueryable<IEvent> Event { get; }
         Task AddEventAsync(IEvent eventEntry);
