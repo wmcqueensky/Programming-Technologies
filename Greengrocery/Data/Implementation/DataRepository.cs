054f31b964ebc8144a421f4767ea26d942fdc7b6
247039 Tymoteusz Piwowarski <247039@edu.p.lodz.pl>
2024-05-26 14:48:04 +0200

    x

--- Greengrocery/Data/Implementation/DataRepository.cs
+++ Greengrocery/Data/Implementation/DataRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Greengrocery.Data.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Greengrocery.Data.Implementation
 {
-    internal class DataRepository
+    public class DataRepository : IDataRepository
     {
+
     }
 }
