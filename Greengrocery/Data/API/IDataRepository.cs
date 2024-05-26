c60110aae240b0e7d0df77b923bcab054b4c1b83
wmcqueensky <mokwinskiwojciech0@gmail.com>
2024-05-26 15:49:39 +0200

    Update IDataRepository

--- Greengrocery/Data/API/IDataRepository.cs
+++ Greengrocery/Data/API/IDataRepository.cs
@@ -9,60 +9,45 @@ namespace Greengrocery.Data.API
 {
     public interface IDataRepository
     {
-        // Methods for User entity
-        Task AddUserAsync(int id, string firstName, string lastName);
-        Task<IUser> GetUserAsync(int id);
-        Task<IUser> GetUserAsyncMethodSyntax(int id);
-        Task UpdateUserAsync(int id, string firstName, string lastName);
-        Task DeleteUserAsync(int id);
-        Task<Dictionary<int, IUser>> GetAllUsersAsync();
-        Task<int> GetUsersCountAsync();
-
-        // Methods for Product entity
-        Task AddProductAsync(int id, string name, string description, decimal price);
-        Task<IProduct> GetProductAsync(int id);
-        Task UpdateProductAsync(int id, string name, string description, decimal price);
-        Task DeleteProductAsync(int id);
+        Task AddProductAsync(int product_id, string name, string type, decimal price);
+        Task<IProduct> GetProductAsync(int product_id);
+        Task UpdateProductAsync(int product_id, string name, string type, decimal price);
+        Task DeleteProductAsync(int product_id);
         Task<Dictionary<int, IProduct>> GetAllProductsAsync();
         Task<int> GetProductsCountAsync();
 
-        // Methods for State entity
-        Task AddStateAsync(int id, int productId, bool available);
-        Task<IState> GetStateAsync(int id);
-        Task UpdateStateAsync(int id, int productId, bool available);
-        Task DeleteStateAsync(int id);
+        Task AddStateAsync(int state_id, int catalog_id);
+        Task<IState> GetStateAsync(int state_id);
+        Task UpdateStateAsync(int state_id, int catalog_id);
+        Task DeleteStateAsync(int state_id);
         Task<Dictionary<int, IState>> GetAllStatesAsync();
         Task<int> GetStatesCountAsync();
 
-        // Methods for Event entity
-        Task AddEventAsync(int id, int stateId, int userId, string type);
-        Task<IEvent> GetEventAsync(int id);
-        Task UpdateEventAsync(int id, int stateId, int userId, string type);
-        Task DeleteEventAsync(int id);
+        Task AddEventAsync(int event_id, int state_id, int employee_id, int supplier_id, int customer_id, string type);
+        Task<IEvent> GetEventAsync(int event_id);
+        Task UpdateEventAsync(int event_id, int state_id, int employee_id, int supplier_id, int customer_id, string type);
+        Task DeleteEventAsync(int event_id);
         Task<Dictionary<int, IEvent>> GetAllEventsAsync();
         Task<int> GetEventsCountAsync();
 
-        // Methods for Employee entity
-        Task AddEmployeeAsync(int id, string firstName, string lastName, decimal salary);
-        Task<IEmployee> GetEmployeeAsync(int id);
-        Task UpdateEmployeeAsync(int id, string firstName, string lastName, decimal salary);
-        Task DeleteEmployeeAsync(int id);
+        Task AddEmployeeAsync(int employee_id, string name, string surname, decimal phone, string email, decimal salary);
+        Task<IEmployee> GetEmployeeAsync(int employee_id);
+        Task UpdateEmployeeAsync(int employee_id, string name, string surname, decimal phone, string email, decimal salary);
+        Task DeleteEmployeeAsync(int employee_id);
         Task<Dictionary<int, IEmployee>> GetAllEmployeesAsync();
         Task<int> GetEmployeesCountAsync();
 
-        // Methods for Supplier entity
-        Task AddSupplierAsync(int id, string name, string email, string phone, int supplierId);
-        Task<ISupplier> GetSupplierAsync(int id);
-        Task UpdateSupplierAsync(int id, string name, string email, string phone, int supplierId);
-        Task DeleteSupplierAsync(int id);
+        Task AddSupplierAsync(int supplier_id, string name, string surname, decimal phone, string email);
+        Task<ISupplier> GetSupplierAsync(int supplier_id);
+        Task UpdateSupplierAsync(int supplier_id, string name, string surname, decimal phone, string email);
+        Task DeleteSupplierAsync(int supplier_id);
         Task<Dictionary<int, ISupplier>> GetAllSuppliersAsync();
         Task<int> GetSuppliersCountAsync();
 
-        // Methods for Customer entity
-        Task AddCustomerAsync(int id, string name, string email, string phone, int customerId, decimal balance);
-        Task<ICustomer> GetCustomerAsync(int id);
-        Task UpdateCustomerAsync(int id, string name, string email, string phone, int customerId, decimal balance);
-        Task DeleteCustomerAsync(int id);
+        Task AddCustomerAsync(int customer_id, string name, string surname, string phone, string email, decimal balance);
+        Task<ICustomer> GetCustomerAsync(int customer_id);
+        Task UpdateCustomerAsync(int customer_id, string name, string surname, string phone, string email, decimal balance);
+        Task DeleteCustomerAsync(int customer_id);
         Task<Dictionary<int, ICustomer>> GetAllCustomersAsync();
         Task<int> GetCustomersCountAsync();
 
