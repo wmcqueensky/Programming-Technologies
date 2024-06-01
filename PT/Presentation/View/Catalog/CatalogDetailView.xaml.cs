dcd74ad2ff82c37c1d3369486b12319889ef39bc
wmcqueensky <mokwinskiwojciech0@gmail.com>
2024-06-02 01:20:21 +0200

    Update View

---
+++ PT/Presentation/View/Catalog/CatalogDetailView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Presentation.View
+{
+    /// <summary>
+    /// Interaction logic for UserDetailView.xaml
+    /// </summary>
+    public partial class UserDetailView : UserControl
+    {
+        public UserDetailView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click()
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
