9a2af44cf20025cdcb9869e1a25ff3e6cad6f83c
247039 Tymoteusz Piwowarski <247039@edu.p.lodz.pl>
2024-06-01 14:06:23 +0200

    product tests

--- PT/DataLayerTests/DataLayerTests.cs
+++ PT/DataLayerTests/DataLayerTests.cs
@@ -19,3 +19,23 @@ namespace DataLayerTests
             Assert.IsTrue(File.Exists(dbPath), $"Database file not found at: {dbPath}");
             testConnectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={dbPath};Integrated Security=True;Connect Timeout=30;";
         }
+
+
+        [TestMethod]
+        public async Task ProductTests()
+        {
+            int productId = 12;
+
+            await _dataRepository.AddProduct(productId, "Moby Dick");
+
+            IProduct product = await _dataRepository.GetProduct(productId);
+
+            Assert.IsNotNull(product);
+            Assert.AreEqual(productId, product.ProductId);
+            Assert.AreEqual("Moby Dick", product.Name);
+
+            Assert.IsNotNull(await _dataRepository.GetAllProducts());
+            //Assert.IsTrue(await _dataRepository.GetProductsCount() > 0);
+
+            await _dataRepository.DeleteProduct(productId);
+        }
